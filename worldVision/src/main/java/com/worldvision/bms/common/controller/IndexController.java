1772e4b25f2ed52917a636ec983245cf31e46870
cheonej95@naver.com <cheonej95@naver.com>
2022-02-04 00:55:45 +0900

    회원가입

--- worldVision/src/main/java/com/worldvision/bms/common/controller/IndexController.java
+++ worldVision/src/main/java/com/worldvision/bms/common/controller/IndexController.java
@@ -1,13 +1,23 @@
 package com.worldvision.bms.common.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.worldvision.bms.repository.UserRepository;
+import com.worldvision.bms.user.model.User;
 
 @Controller
 public class IndexController {
-
-
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	@GetMapping({ "", "/" })
 	public @ResponseBody String index() {
@@ -26,17 +36,22 @@ public class IndexController {
 	
 	@GetMapping("/login")
 	public String login() {
-		return "login";
+		return "/user/login";
 	}
 
-	@GetMapping("/join")
-	public @ResponseBody String join() {
-		return "join";
+	@GetMapping("/userCreate")
+	public String join() {
+		return "/user/userCreate";
 	}
 
-	@GetMapping("/joinProc")
-	public @ResponseBody String joinProc() {
-		return "joinProc";
+	@PostMapping("/userCreate")
+	public String userCreate(User user) {
+		System.out.println("안녕"+user);
+		String rawPassword = user.getPassword();
+		String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+		user.setPassword(encPassword);
+		userRepository.save(user);
+		return "redirect:/login";
 	}
 
 }
